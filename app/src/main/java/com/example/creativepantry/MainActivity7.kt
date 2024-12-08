0f3883cedd2433d4c1e5d451c441a42d4b971988
alep <alpimarti10@gmail.com>
2024-12-08 16:13:20 +0100

    refactoritzant

--- app/src/main/java/com/example/creativepantry/MainActivity7.kt
+++ app/src/main/java/com/example/creativepantry/MainActivity7.kt
@@ -11,10 +11,5 @@ class MainActivity7 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main7)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
