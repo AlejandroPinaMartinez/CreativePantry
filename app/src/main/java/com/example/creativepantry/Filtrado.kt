5537ee9df1e95e31771a8a12fdf9525586ac612c
Juandiaz <juandica2005@gmail.com>
2025-01-23 10:53:31 +0100

    Avanzando el RecyclerView

--- app/src/main/java/com/example/creativepantry/Filtrado.kt
+++ app/src/main/java/com/example/creativepantry/Filtrado.kt
@@ -3,17 +3,37 @@ package com.example.creativepantry
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class Filtrado : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recipeAdapter: RecipeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_filtrado)
+
+        //recyclerView = findViewById(R.id.recyclerView)
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val listaRecetas = cargarRecetas()
+        recipeAdapter = RecipeAdapter(listaRecetas)
+        recyclerView.adapter = recipeAdapter
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.bottom_bar_container, BarraInferiorOpcions())
             .commit()
         supportFragmentManager.beginTransaction()
             .replace(R.id.top_bar_container, FragmentBarraSuperiorOpciones())
             .commit()
+    }
+
+    private fun cargarRecetas(): List<Receta> {
+        return listOf(
+            Receta("Arroz a la cubana", 4.8f, 20, "@drawable/plato1"),
+            Receta("Bacalao", 3.3f, 30, "@drawable/plato2")
+        )
     }
 }
