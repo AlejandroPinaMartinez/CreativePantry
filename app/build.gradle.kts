a17190780879a26d80a4e27df7d1d7d43f982ae8
juandiaz21 <2023_juan.diaz@gmail.com>
2025-04-01 19:25:57 +0200

    Avanç Grafics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
 
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 
     // Retrofit y Gson Converter
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
